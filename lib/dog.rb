20603791ad012bc1331433a1d0e33a77a2698181
Stavroula Guerrero <4stacyg@optonline.net>
2017-12-20 17:12:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog
 
-  def initialize(dogs_name)
-    @name=dogs_name
+  def initialize(name)
+    @name = name
   end
-  
+
   def name
     @name
   end
-  
+
   def initialize(breed = "Mutt")
     @breed = breed
   end
